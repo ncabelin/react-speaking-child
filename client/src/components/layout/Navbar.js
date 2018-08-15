2def3940fe1264eff0920174894786153f4a20b0
Michael Cabelin <ncabelin@gmail.com>
2018-08-14 23:20:37 -0700

    add old navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,9 +3,27 @@ import { Link } from 'react-router-dom';
 
 export default () => {
   return (
-    <nav className="">
-      <Link to="/register">Register</Link> 
-      <Link to="/login">Login</Link>
-    </nav>
+      <nav className="navbar navbar-default">
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <Link to="/"><img src="images/speakingchild_logo2.png" className="navbar-brand logo" /></Link>
+            </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav navbar-right">
+                <li><Link to="/" className="ng-binding">Child: </Link></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/login">Sign in</Link></li>
+                <li><Link to="/register">Register</Link></li>
+                <li><Link to="/">Logout</Link></li>
+              </ul>
+            </div>
+          </div>
+      </nav>
   )
 }
