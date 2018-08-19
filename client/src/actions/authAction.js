beb9d616b1d62c579879b358a663620440270a54
Michael Cabelin <ncabelin@gmail.com>
2018-08-19 01:34:11 -0700

    fix login, style feedback

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -28,6 +28,7 @@ export const loginUser = userData => dispatch => {
       dispatch(setCurrentUser(decoded));
     })
     .catch(err => {
+      console.log(err);
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
