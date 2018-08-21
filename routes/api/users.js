617e52526a6a88cfe6445dee952b8463a4c6d0e5
Michael Cabelin <ncabelin@gmail.com>
2018-08-21 00:31:32 -0700

    rename name to childname

--- routes/api/users.js
+++ routes/api/users.js
@@ -71,7 +71,7 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if (isMatch) {
-            const payload = { id: user.id, name: user.name };
+            const payload = { id: user.id, childname: user.childname };
             jwt.sign(
               payload,
               keys.secret,
